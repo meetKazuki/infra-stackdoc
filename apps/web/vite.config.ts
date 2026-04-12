31455a799bda2bdc9b974fdd222dda29f85fa403
Desmond Edem <edemintech@gmail.com>
2026-04-12 20:03:29 +0100

    fix: fix api routing

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -30,4 +30,12 @@ export default defineConfig({
       },
     },
   },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
 })
